59f81655b3d259edef9cec9faea812fa4727bed8
qjerap <romainpareja@gmail.com>
2021-08-25 19:13:07 -0400

    global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-import "../styles/globals.css";
+import "../styles/main.scss";
 
 import { Provider as AuthProvider } from "next-auth/client";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "../redux/store/store";
 
 export default function App({ Component, pageProps }) {
-
   return (
     <ReduxProvider store={store}>
       <AuthProvider session={pageProps.session}>
